a669810538b97f1886503f18f8cbdf55f286cee8
jamesnitsch <James.Nitsch@WillowtreeApps.com>
2020-05-08 13:09:27 -0400

    Add OkHttp, extend timeout on retrofit service

--- app/src/main/java/com/seank/kotlinflowplayground/di/ApplicationModule.kt
+++ app/src/main/java/com/seank/kotlinflowplayground/di/ApplicationModule.kt
@@ -5,14 +5,14 @@ import android.content.Context
 import com.seank.kotlinflowplayground.data.CardsRepository
 import com.seank.kotlinflowplayground.data.CardsRepositoryImpl
 import com.seank.kotlinflowplayground.data.CardsService
-import com.seank.kotlinflowplayground.flow.DefaultDispatcherProvider
-import com.seank.kotlinflowplayground.flow.DispatcherProvider
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -35,8 +35,15 @@ class ApplicationModule(val application: Application) {
     @Provides
     @Singleton
     fun providesCardsService(moshi: Moshi) : CardsService {
+        val okHttpClient = OkHttpClient
+            .Builder()
+            .readTimeout(60L, TimeUnit.SECONDS)
+            .connectTimeout(60L, TimeUnit.SECONDS)
+            .build()
+
         return Retrofit.Builder()
             .baseUrl("https://api.magicthegathering.io")
+            .client(okHttpClient)
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build().create(CardsService::class.java)
     }

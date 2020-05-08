20da26be696aa5598fd83e17d7ab6b87df0a4600
Sean Kenkeremath <sean.kenkeremath@willowtreeapps.com>
2020-05-08 01:53:42 -0400

    Add example viewmodel test

---
+++ app/src/test/java/com/seank/kotlinflowplayground/main/MainViewModelTest.kt
@@ -0,0 +1,60 @@
+package com.seank.kotlinflowplayground.main
+
+import CoroutineTestRule
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.seank.kotlinflowplayground.data.CardsRepository
+import com.seank.kotlinflowplayground.domain.Card
+import kotlinx.coroutines.flow.flow
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations.initMocks
+
+class MainViewModelTest {
+    @get:Rule
+    val instantTaskExecutorRule: InstantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    var coroutinesTestRule = CoroutineTestRule()
+
+    @Mock
+    private lateinit var cardsRepository: CardsRepository
+
+    @Mock
+    private lateinit var cardsObserver: Observer<List<Card>>
+
+    @Mock
+    private lateinit var showContentObserver: Observer<Boolean>
+
+    private lateinit var viewModel: MainViewModel
+
+    @Before
+    fun setUp() {
+        initMocks(this)
+        `when`(cardsRepository.getCards()).thenReturn(flow {
+            emit(listOf(Card(name = "Test Card", imgUrl = "img_url")))
+        })
+        viewModel = MainViewModel(cardsRepository)
+        viewModel.cards.observeForever(cardsObserver)
+        viewModel.showContent.observeForever(showContentObserver)
+    }
+
+    @Test
+    fun `when fetchCard then loading set to false then true when content loads`() {
+        viewModel.fetchCard()
+        val showContentArgumentCaptor = argumentCaptor<Boolean>()
+        verify(showContentObserver, atLeastOnce()).onChanged(showContentArgumentCaptor.capture())
+        assert(showContentArgumentCaptor.allValues[0] == false)
+        assert(showContentArgumentCaptor.allValues[1] == true)
+
+        val cardsArgumentCaptor = argumentCaptor<List<Card>>()
+        verify(cardsObserver).onChanged(cardsArgumentCaptor.capture())
+        assert(cardsArgumentCaptor.allValues.size == 1)
+        assert(cardsArgumentCaptor.allValues[0].size == 1)
+        assert(cardsArgumentCaptor.lastValue[0] == Card(name = "Test Card", imgUrl = "img_url"))
+    }
+}
